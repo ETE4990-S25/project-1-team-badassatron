4f16dcab6fac6d9d2569ca06c28b5cd0a17d3447
Grace Brisco <grakei.brisco@gmail.com>
2025-03-13 16:11:26 -0700

    layout confirm

---
+++ badassatronmain.py
@@ -0,0 +1,9 @@
+#game start
+#Phase 1: Character Selection (with traits and strengths)
+#Phase 2: Establish Inventory (dictionary with items and traits)
+#Phase 3: First Event Sequence
+#Phase 4: Event Path 1
+#Phase 5: Event Path 2
+#Phase 6: Event Path 3
+#Phase 7: Event Path 4
+#Game End with Stats -> Reset and Restart
