a251e3cce1c6d09e34fc80c1ff5cb12563fd17d3
grakeidile <grakei.brisco@gmail.com>
2025-03-15 15:13:25 -0700

    menustart

--- badassatronmain.py
+++ badassatronmain.py
@@ -1,4 +1,20 @@
 #game start
+import json
+
+def mainmenu():
+    print('-----Savior of Cybertron----')
+    print('Hello citizen, Crybertron needs your help. ')
+    print('We are sending you on a mission to discover the who has been messing with the core of our hom')
+    playername = input('First, please tell us who you are: ')
+    print('Now, we need to create your character. ')
+    characterselection()
+
+def characterselection():
+    print('Where do you lie, (choices) ') #put choices of character class warrior minor officer etc.
+                                        #make like hw 3
+
+if __name__== '__main__':
+    mainmenu()
 #Phase 1: Character Selection (with traits and strengths)
 #Phase 2: Establish Inventory (dictionary with items and traits)
 #Phase 3: First Event Sequence
