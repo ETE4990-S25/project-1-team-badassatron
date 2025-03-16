91718458d014e345ee9a9f5b8c186438bc84168d
Jayvee Bonus <jmbonus@cpp.edu>
2025-03-15 22:34:34 -0700

    bump

--- badassatronmain.py
+++ badassatronmain.py
@@ -1,6 +1,27 @@
-#game start
+#game start 
+#Game Title: War for Cybertron
+#Game Description: Turn based RPG played as a Cybertronian in the midst of the Autobot and Decepticon War.
 #Phase 1: Character Selection (with traits and strengths)
+class Player:
+    def _init_(self, name, character_class):
+        self.name = name
+        self.character_class = character_class
+        self.health = 100
+        self.attack = 0
+        self.heal = 0
+        self.inventory = []
+        self.set_attributes(character_class)
+
+    def set_attributes(self, character_class):
+        if character_class == "medic":
+            self.heal = 20
+        elif character_class == "warrior"
+            self.attack = 20
+
+    def show_profile(self):
+        return f"Name: {self.name}, Class: {self.character_class}, HP: {self.health}, Attack: {self.attack}, Heal: {self.heal}"
 #Phase 2: Establish Inventory (dictionary with items and traits)
+
 #Phase 3: First Event Sequence
 #Phase 4: Event Path 1
 #Phase 5: Event Path 2
